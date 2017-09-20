85130a1c6ac2b3dd732e809941295d25ea45daf7
edgardo vasquez <edgardo.a.v@gmail.com>
2017-09-20 18:18:12 -0300

    Se cambia titulo modulo 35 a 35_Sesiones

--- 35_Manejo y validacion de peticiones HTTP/testAPIconsumerAnimales/main.js
+++
@@ -1,8 +0,0 @@
-var Cliente = require("./cliente.js");
-
-var cliente = new Cliente("localhost", "3000", "http");
-
-cliente.post("/animales/", {
-    nombre: "Luna",
-    especie: "perro"
-}, (respuesta) => console.log(respuesta));
--- 35_Sesiones/backend/routes/animales.js
+++ 35_Sesiones/backend/routes/animales.js
@@ -1,13 +1,13 @@
 //Agrego los modulos para convertir este .js en una ruta para express
 var express = require('express');
-var router = express.Router(); //Para hacer uso de las rutas de node
+var router = express.Router();//Para hacer uso de las rutas de node
 
-var contadorVisitas = function(req, res, next) {
+var contadorVisitas = function(req,res,next){
   if (req.session.vistas == undefined) {
-    req.session.vistas = 0;
+    req.session.vistas=0;
   }
   req.session.vistas++;
-  next(); //Callback, se le prodrian pasar funciones u otros parametros
+  next();//Callback, se le prodrian pasar funciones u otros parametros
   //next indica que se ejecuten las siguientes funciones
 };
 //Middleware, parte del codigo se ejecuta en router antes de los otros metodos de router.
@@ -15,36 +15,10 @@ var contadorVisitas = function(req, res, next) {
 //puede ser usado para validar sessiones u otras acciones previas a las respuestas al cliente
 router.use(contadorVisitas);
 
-router.get('/', (req, res, next) => {
-  console.log(req.session); //Para obtener la cookie que contiene la session
+router.get('/',(req,res,next)=>{
+  console.log(req.session);//Para obtener la cookie que contiene la session
   res.send("Recurso de animales ha sido visto " + req.session.vistas + " veces");
 });
-
-//Crear un animal
-router.post('/', (req, res, next) => {
-  console.log(req.body);
-
-  //Guardarlo en memoria
-  if (!req.session.animales) {
-    req.session.animales = [];
-  }
-
-  var body = req.body;
-  // {nombre, especie}
-  if (req.get('content-type') == "application/json") {
-  //if (req.get('content-type') == "text/plain") {
-    console.log("Validado")
-  } else {
-    res.status(403);
-    res.send("ERROR");
-  }
-
-
-  req.session.animales.push(req.body);
-  //console.log(req.session);
-  res.send("Peticion ha sido recibida");
-});
-
 
 //Exporto el modulo para que pueda ser usado como ruta en la app principal
 module.exports = router;
