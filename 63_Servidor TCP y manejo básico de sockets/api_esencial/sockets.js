012b048bcc5b9b5808c03e8fe96c9ee5be06a458
edgardo vasquez <edgardo.a.v@gmail.com>
2017-11-10 15:23:24 -0300

    create 63_Servidor TCP y manejo básico de sockets

---
+++ 63_Servidor TCP y manejo básico de sockets/api_esencial/sockets.js
@@ -0,0 +1,19 @@
+// sockets - TCP
+var net = require("net");
+
+var connecciones =[];
+
+var servidor = net.createServer((socket) => {
+    socket.write("Server: echo..:\n");
+    connecciones.push(socket);
+    socket.on('data', (d) => {
+      //socket.write("no me gusta tu mensaje: " + d);
+      connecciones.forEach((s)=>{
+        s.write(d);
+      });
+    });
+    socket.on('close', ()  => {
+        console.log("Cliente desconectado...");
+    });
+});
+servidor.listen(1234, () => {console.log("Servidor escuchando clientes...")});
